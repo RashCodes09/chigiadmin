58e0988f2f587a5e11b1bb13a861fb3cf4b3e3ac
RashCodes09 <ayinkerasheedat500@gmail.com>
2024-07-14 13:39:37 +0100

    made changes

--- app/utlis/constant.ts
+++
@@ -1,4 +0,0 @@
-// import dontenv from "dontenv"
-// dontenv.config()
-
-// export const DB_URL = process.env.URL as string;
--- app/utlis/dbConfig.ts
+++ app/utlis/dbConfig.ts
@@ -1,14 +1,23 @@
 import { connect } from "mongoose";
 
+// const dbConfig = async () => {
+//   const url = " mongodb://localhost:27017/cmsDB";
+//   try {
+//     await connect(url).then(() => {
+//       console.clear();
+//       console.log("CMS connected");
+//     });
+//   } catch (error: any) {
+//     console.error(error);
+//   }
+// };
+
 const dbConfig = async () => {
-  try {
-    await connect("mongodb://localhost:27017/cmsDB").then(() => {
-      console.clear();
-      console.log("CMS connected");
-    });
-  } catch (error: any) {
-    console.error(error);
-  }
+  const url = "mongodb://localhost:27017/cmsDB";
+  await connect(url).then(() => {
+    console.clear();
+    console.log("Server Up!");
+  });
 };
 
 export default dbConfig;
